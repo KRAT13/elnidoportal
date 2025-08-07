88019927a93f4d9ddb099d18190af5b6a16a6080
Ryan G. Sanchez <krat1324@gmail.com>
2025-08-08 01:29:02 +0800

    Add About Me

--- src/app/components/home/about/about.component.ts
+++ src/app/components/home/about/about.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { LogoComponent } from '../../../shared/components/logo/logo.component';
+import * as AOS from 'aos';
 
+AOS.init({
+  duration: 800,
+  once: true,
+});
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [LogoComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  private fbProfileId = '100001725262522';
+  private fbUsername = 'tark13com';
 
+  openFacebookProfile(): void {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    if (isMobile) {
+      window.location.href = `fb://profile/${this.fbProfileId}`;
+      setTimeout(() => {
+        window.location.href = `https://www.facebook.com/${this.fbUsername}`;
+      }, 2000);
+    } else {
+      window.location.href = `https://www.facebook.com/${this.fbUsername}`;
+    }
+  }
+
+  openMessenger(): void {
+    const timeout = setTimeout(() => {
+      window.open(`https://m.me/${this.fbUsername}`, '_blank');
+    }, 1500);
+
+    window.location.href = `fb-messenger://user-thread/${this.fbUsername}`;
+  }
+
+  openWebsite(): void {
+  window.open('https://www.tark13.com', '_blank');
+}
 }
