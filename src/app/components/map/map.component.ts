079a4e7a9352509a1377e440254e6e3b2323b6a1
Ryan G. Sanchez <krat1324@gmail.com>
2025-08-12 01:37:00 +0800

    Update design for menu and change mapping library

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,68 +1,108 @@
 import { AfterViewInit, Component, inject, OnInit } from '@angular/core';
-import * as L from 'leaflet';
+// import * as L from 'leaflet';
+import { icon, latLng, Map, marker, Marker, tileLayer } from 'leaflet';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+
 import { LocationService } from '../../services';
 @Component({
   selector: 'app-map',
-  imports: [],
+  imports: [LeafletModule],
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
 export class MapComponent implements OnInit, AfterViewInit  {
-  private map!: L.Map;
-  private myLocationMarker!: L.Marker;
-  
-  lat?: number;
-  lng?: number;
+  // private map!: L.Map;
+  // private myLocationMarker!: L.Marker;
+  // markers: L.Marker[] = [];
+  // lat?: number;
+  // lng?: number;
+
+  map!: Map;
+  markers: Marker[] = [];
+  options = {
+    layers: [
+      tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+        attribution: 'Google Maps'
+      })
+    ],
+    zoom: 12,
+    center: latLng(11.179903, 119.390459)
+  };
 
   locationService = inject( LocationService)
-
-  ngAfterViewInit(): void {
-    const customIcon = L.icon({
+  customIcon = icon({
       iconUrl: '/assets/images/map/pin.svg',
       iconSize: [38, 38],
       iconAnchor: [19, 38],
       popupAnchor: [0, -38]
     });
+  ngAfterViewInit(): void {
 
-    this.map = L.map('map', { zoomControl: false })
-      .setView([11.179903, 119.390459], 20);
 
-    L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-      maxZoom: 20,
-      subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-    }).addTo(this.map);
+    // this.map = L.map('map', { zoomControl: false })
+    //   .setView([11.179903, 119.390459], 20);
 
-    // Static location marker
-    L.marker([11.179903, 119.390459], { icon: customIcon })
-      .addTo(this.map)
-      .bindTooltip('El Nido Municipality', { permanent: false, direction: 'auto', className: 'customMapTooltip'});
+    // L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    //   maxZoom: 20,
+    //   subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    // }).addTo(this.map);
 
-    // Zoom control bottom right
-    L.control.zoom({ position: 'bottomright' }).addTo(this.map);
+    // // Static location marker
+    // L.marker([11.179903, 119.390459], { icon: customIcon })
+    //   .addTo(this.map)
+    //   .bindTooltip('El Nido Municipality', { permanent: false, direction: 'auto', className: 'customMapTooltip'});
+
+    // // Zoom control bottom right
+    // L.control.zoom({ position: 'bottomright' }).addTo(this.map);
   }
 
+  onMapReady(map: Map) {
+    this.map = map;
+    
+    // Add some markers
+    // const londonMarker = marker([11.179903, 119.390459]).addTo(this.map);
+    
+    // // Store markers for reference
+    // this.markers = [londonMarker];
+    
+    // // Add click handlers to each marker
+    // this.markers.forEach(m => {
+    //   m.on('click', (e) => {
+    //     this.centerAndZoom(e.target);
+    //   });
+
+    //   m.setIcon(this.customIcon);
+    // });
+
+    this.addMarker(11.179903, 119.390459, 'El Nido Municipality')
+  }
+
+  addMarker(lat: number, lng: number, title: string) {
+    const newMarker = marker([lat, lng], {icon: this.customIcon}).addTo(this.map).bindTooltip(title);
+    
+    newMarker.on('click', (e) => {
+      this.centerAndZoom(e.target);
+    });
+
+    this.markers.push(newMarker);
+  }
+
+  centerAndZoom(marker: L.Marker) {
+    if (this.map) {
+      // this.map.setView(marker.getLatLng(), 20);
+      this.map.flyTo(marker.getLatLng(), 17);
+    }
+  }
 
   findMe() {
     this.locationService.getCurrentLocation()
       .then(pos => {
-        this.lat = pos.coords.latitude;
-        this.lng = pos.coords.longitude;
+        const lat = pos.coords.latitude;
+        const lng = pos.coords.longitude;
 
-        const locationIcon = L.icon({
-          iconUrl: '/assets/images/marker-icon.png', // <-- use your own icon
-          iconSize: [32, 32],
-          iconAnchor: [16, 32],
-        });
-
-        this.myLocationMarker = L.marker([this.lat, this.lng], { icon: locationIcon })
-          .addTo(this.map)
-          .bindTooltip('El Nido Municipality', { permanent: false, direction: 'auto' })
-          .on('click', () => {
-            this.onMarkerClick();
-          });
-
-        // Center map on current location
-        this.map.setView([this.lat, this.lng], 18);
+        this.addMarker(lat, lng, 'My Location');
       })
       .catch(err => {
         console.error(err);
@@ -70,8 +110,8 @@ export class MapComponent implements OnInit, AfterViewInit  {
       });
   }
   
-  onMarkerClick() {
-    console.log('Marker Clicked!');
+  onMarkerClick(e: any) {
+    this.centerAndZoom(e)
   }
 
   ngOnInit(): void {
